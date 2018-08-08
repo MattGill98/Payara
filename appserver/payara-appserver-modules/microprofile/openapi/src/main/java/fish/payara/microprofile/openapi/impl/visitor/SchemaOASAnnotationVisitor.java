09c14524b1385c4ccea881573db24372c59a1e07
Matt Gill <matthew.gill@live.co.uk>
2018-08-08 12:35:54 +0100

    Added most schema properties.

--- appserver/payara-appserver-modules/microprofile/openapi/src/main/java/fish/payara/microprofile/openapi/impl/visitor/SchemaOASAnnotationVisitor.java
+++ appserver/payara-appserver-modules/microprofile/openapi/src/main/java/fish/payara/microprofile/openapi/impl/visitor/SchemaOASAnnotationVisitor.java
@@ -1,29 +1,115 @@
 package fish.payara.microprofile.openapi.impl.visitor;
 
+import java.math.BigDecimal;
+import java.util.logging.Logger;
+
 import org.eclipse.microprofile.openapi.models.media.Schema;
 import org.eclipse.microprofile.openapi.models.media.Schema.SchemaType;
 import org.glassfish.hk2.external.org.objectweb.asm.AnnotationVisitor;
 
 public class SchemaOASAnnotationVisitor extends OASAnnotationVisitor {
 
-    private Schema currentSchema;
+    private static final Logger LOGGER = Logger.getLogger(SchemaOASAnnotationVisitor.class.getName());
+
+    private final Schema currentSchema;
+
+    private String arrayName;
 
     public SchemaOASAnnotationVisitor(OASContext context, Schema currentSchema) {
         super(context);
         this.currentSchema = currentSchema;
     }
 
+    public SchemaOASAnnotationVisitor(OASContext context, Schema currentSchema, String arrayName) {
+        this(context, currentSchema);
+        this.arrayName = arrayName;
+    }
+
     @Override
     public void visit(String name, Object value) {
-        if ("description".equals(name)) {
-            currentSchema.setDescription(value.toString());
+        if (arrayName != null) {
+            name = arrayName;
+        } if (name != null) {
+            switch (name) {
+                case "description":
+                    currentSchema.setDescription(value.toString());
+                    break;
+                case "title":
+                    currentSchema.setTitle(value.toString());
+                    break;
+                case "multipleOf":
+                    currentSchema.setMultipleOf(new BigDecimal(value.toString()));
+                    break;
+                case "maximum":
+                    currentSchema.setMaximum(new BigDecimal(value.toString()));
+                    break;
+                case "minimum":
+                    currentSchema.setMinimum(new BigDecimal(value.toString()));
+                    break;
+                case "exclusiveMaximum":
+                    currentSchema.setExclusiveMaximum(Boolean.valueOf(value.toString()));
+                    break;
+                case "exclusiveMinimum":
+                    currentSchema.setExclusiveMinimum(Boolean.valueOf(value.toString()));
+                    break;
+                case "maxLength":
+                    currentSchema.setMaxLength(Integer.valueOf(value.toString()));
+                    break;
+                case "minLength":
+                    currentSchema.setMinLength(Integer.valueOf(value.toString()));
+                    break;
+                case "maxProperties":
+                    currentSchema.setMaxProperties(Integer.valueOf(value.toString()));
+                    break;
+                case "minProperties":
+                    currentSchema.setMinProperties(Integer.valueOf(value.toString()));
+                    break;
+                case "maxItems":
+                    currentSchema.setMaxItems(Integer.valueOf(value.toString()));
+                    break;
+                case "minItems":
+                    currentSchema.setMinItems(Integer.valueOf(value.toString()));
+                    break;
+                case "pattern":
+                    currentSchema.setPattern(value.toString());
+                    break;
+                case "format":
+                    currentSchema.setFormat(value.toString());
+                    break;
+                case "nullable":
+                    currentSchema.setNullable(Boolean.valueOf(value.toString()));
+                    break;
+                case "readOnly":
+                    currentSchema.setReadOnly(Boolean.valueOf(value.toString()));
+                    break;
+                case "writeOnly":
+                    currentSchema.setWriteOnly(Boolean.valueOf(value.toString()));
+                    break;
+                case "example":
+                    currentSchema.setExample(value);
+                    break;
+                case "defaultValue":
+                    currentSchema.setDefaultValue(value);
+                    break;
+                case "deprecated":
+                    currentSchema.setDeprecated(Boolean.valueOf(value.toString()));
+                    break;
+                case "requiredProperties":
+                    currentSchema.addRequired(value.toString());
+                    break;
+                case "enumeration":
+                    currentSchema.addEnumeration(value);
+                    break;
+                default:
+                    LOGGER.info(String.format("Unrecognised property: '%s'.", name));
+            }
         }
         super.visit(name, value);
     }
 
     @Override
     public AnnotationVisitor visitArray(String name) {
-        return super.visitArray(name);
+        return new SchemaOASAnnotationVisitor(context, currentSchema, name);
     }
 
     @Override
